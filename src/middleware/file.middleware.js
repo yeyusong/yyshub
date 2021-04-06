85d427bc4a986d286a631383bb42614e1e014c76
yeyusong <1799474409@qq.com>
2021-04-06 13:29:19 +0800

    📝 即将使用jimp

--- src/middleware/file.middleware.js
+++ src/middleware/file.middleware.js
@@ -2,7 +2,7 @@
  * @Author: yeyusong
  * @Date: 2021-03-31 14:33:13
  * @LastEditors: yeyusong
- * @LastEditTime: 2021-04-06 10:09:39
+ * @LastEditTime: 2021-04-06 13:23:30
  * @Description:
  */
 const Multer = require('koa-multer')
@@ -20,4 +20,10 @@ const pictureUpload = Multer({
 
 const pictureHandler = pictureUpload.array('picture', 9)
 
-module.exports = { avatarHandler, pictureHandler }
+const pictureResize = async (ctx, next) => {
+  const file = ctx.req.files
+  for (let file of files) {
+  }
+}
+
+module.exports = { avatarHandler, pictureHandler, pictureResize }
--- src/router/file.router.js
+++ src/router/file.router.js
@@ -2,7 +2,7 @@
  * @Author: yeyusong
  * @Date: 2021-03-31 14:06:11
  * @LastEditors: yeyusong
- * @LastEditTime: 2021-04-06 10:11:00
+ * @LastEditTime: 2021-04-06 13:13:22
  * @Description:
  */
 const Router = require('koa-router')
@@ -11,6 +11,7 @@ const { verifyAuth } = require('../middleware/auth.middleware')
 const {
   avatarHandler,
   pictureHandler,
+  pictureResize,
 } = require('../middleware/file.middleware')
 const {
   saveAvatarInfo,
@@ -20,6 +21,12 @@ const {
 const fileRouter = new Router({ prefix: '/upload' })
 
 fileRouter.post('/avatar', verifyAuth, avatarHandler, saveAvatarInfo)
-fileRouter.post('/picture', verifyAuth, pictureHandler, savePictureInfo)
+fileRouter.post(
+  '/picture',
+  verifyAuth,
+  pictureHandler,
+  pictureResize,
+  savePictureInfo
+)
 
 module.exports = fileRouter
